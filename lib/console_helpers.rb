700ff29d78e11457841b5215eba4670e450a198d
calebanderson <caleb.r.anderson.1@gmail.com>
2023-01-14 22:42:25 -0500

    Removing files that were transferred to their own gems

--- lib/console_helpers.rb
+++ lib/console_helpers.rb
@@ -3,9 +3,6 @@ require "console_helpers/engine"
 
 module ConsoleHelpers
   class << self
-    CONSOLE_WIDTH_RANGE = (100..500).freeze
-    DEFAULT_CONSOLE_WIDTH = 250
-
     attr_reader :logger
 
     def mount(object)
@@ -19,7 +16,7 @@ module ConsoleHelpers
     end
 
     def reload
-      Reloader.initialized!
+      ReloaderHooks.initialized!
       silence_warnings { files.each(&method(:load)) }
       extend self
     end
@@ -31,35 +28,6 @@ module ConsoleHelpers
 
     def log(msg)
       @logger.add(msg)
-    end
-
-    def width
-      @width ||= DEFAULT_CONSOLE_WIDTH
-    end
-
-    def config_width(given_width = nil)
-      input_width = given_width || _get_input_width
-      return true if input_width.zero?
-
-      if CONSOLE_WIDTH_RANGE.cover?(input_width)
-        @width = input_width
-        print('New ') & display_width if given_width.nil?
-        true
-      else
-        puts "Width #{input_width} not within accepted range (#{CONSOLE_WIDTH_RANGE})"
-      end
-    end
-
-    def _get_input_width
-      display_width
-      puts 'Press enter/return to accept. Enter a width or characters up to the desired limit, then press enter to set.'
-      input = gets.chomp
-      input.size < CONSOLE_WIDTH_RANGE.min && input[/^\d+$/] ? input.to_i : input.size
-    end
-
-    def display_width
-      puts "Console Width: #{width}"
-      puts '-' * width.to_i
     end
   end
 end
